f7bf1f7ffda3e2d290f67c9f7e92e69834792995
Aleksander Cehovin <alek.it@protonmail.com>
2025-10-25 15:08:03 +0200

    Basic model class for cleaner jupyther script

--- lab1/lib/model.py
+++ lab1/lib/model.py
@@ -2,9 +2,24 @@ import tensorflow as tf
 import numpy as np
 import matplotlib.pyplot as plt
 
+def getDefaultParams():
+    params = {"model":'linear',
+              "train":{ "X":np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0]),
+                         "Y":np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0])
+                      },
+              "test":{
+                         "X":np.array([-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+                     }
+             }
+    return params
+
 class LabModel:
 
-    def __init__(self):
+    def __init__(self, params=None):
+        self.params=params
+        if self.params is None:
+            self.params = getDefaultParams()
+        assert self.params["model"] == 'linear'
         self.model = tf.keras.models.Sequential([
                                 tf.keras.layers.Dense(units=2, input_dim=1, activation='linear'),
                                 tf.keras.layers.Dense(units=1, activation='linear')
@@ -14,15 +29,15 @@ class LabModel:
 
     def train(self):
         #Formula: Y = 2X – 1
-        self.train_input_X   = np.array([-1.0,  0.0, 1.0, 2.0, 3.0, 4.0])
-        self.train_output_Y  = np.array([-3.0, -1.0, 1.0, 3.0, 5.0, 7.0])
+        self.train_input_X   = self.params["train"]["X"]
+        self.train_output_Y  = self.params["train"]["Y"]
 
         #Train the model
         #model.fit(train_input_X, train_output_Y, epochs=5)
         self.model.fit(self.train_input_X, self.train_output_Y, epochs=50)
 
         #Test the model
-        self.test_input_X = np.array([-3.0, -2.0, -1.0, 0.0, 1.0, 2.0, 3.0, 4.0, 5.0, 6.0, 7.0, 8.0, 9.0, 10.0])
+        self.test_input_X = self.params["test"]["X"]
         self.test_prediction_Y = self.model.predict(self.test_input_X)
 
 
